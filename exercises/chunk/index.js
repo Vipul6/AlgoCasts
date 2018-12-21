a9e3f7a5d439d3be356e8747b12cd6f1aea89ecd
saurabhchaudhary3613 <saurabh3613@yahoo.com>
2018-12-21 16:05:19 +0530

    array chuking

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,37 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chuncked= [];
+    let index = 0;
+
+    while(index < array.length) {
+        chuncked.push(array.slice(index, index + size));
+        index += size;
+    }
+  
+  return chuncked
+}
+  
+console.log(chunk([1,2,3,4,5,6,7], 2));
+
+//------------Solution 1
+// function chunk(array, size) {
+//     // console.log(array, size);
+//     const chuncked= [];
+    
+//     for (let ele of array ){
+//       const last = chuncked[chuncked.length - 1];
+      
+//       if(!last || last.length === size) {
+//         chuncked.push([ele]);
+//       }else {
+//         last.push(ele);
+//       }
+//     }
+//     return chuncked;
+//  }
+  
+// console.log(chunk([1,2,3,4,5,6,7], 2));
 
 module.exports = chunk;
