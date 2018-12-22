c46d0b11c38ec4ac56f085cd0eaf8caac60b83e3
saurabhchaudhary3613 <saurabh3613@yahoo.com>
2018-12-22 18:10:09 +0530

    capitalize problem

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,38 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
 
-module.exports = capitalize;
+//--------- Solution 2
+
+function capitalize(str) {
+    let result = str[0].toUpperCase();
+    
+    
+    for(let i=1; i < str.length; i++) {
+      
+      if(str[i - 1] === ' ') {
+        result += str[i].toUpperCase();
+      }else {
+        result += str[i]
+      }
+      
+    }
+}
+
+module.exports = capitalize;  
+
+//------------ Solution 1
+
+// function capitalize(str) {
+//     const words = [];
+  
+//     for(let char of str.split(' ')) {
+//       words.push(char[0].toUpperCase() + char.slice(1));
+//     }
+    
+//     return words.join(' ');
+    
+    
+// }
+
+
